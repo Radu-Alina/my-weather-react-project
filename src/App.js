c170bca909db4dcc5db2495c03d9ea79016a8012
Radu-Alina <alinamomirov@gmail.com>
2022-11-02 12:44:14 +0100

    Added the Git repo URL

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://alinaradu.art/" target="_blank">Alina
+        Radu </a>{""} and is{" "}
+        <a
+          href="https://github.com/Radu-Alina/my-weather-react-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
